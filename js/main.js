85618cefee33384d0ba4d4bd4297dbd48ab7201e
Sujit <its.suzit@gmail.com>
2019-07-31 21:06:52 +1000

    Layout complete push

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ $(".menu").on("click", function() {
   $(this).toggleClass("active");
   $(".overlay").toggleClass("menu-open");
 });
+
+$(".nav-link").on("click", function() {
+  $(".overlay").removeClass("menu-open");
+  $(".menu").removeClass("active");
+});
